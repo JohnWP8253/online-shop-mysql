f7c0d15c871abfcf02c3ab9f4a29ee819385fbc0
John Pendergrass <john.pendergrass@me.com>
2020-04-21 13:41:22 -0500

    added table showing products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var prompt = require("prompt");
 var inquirer = require("inquirer");
+var Table = require("cli-table3");
 
 // connection to mysql database
 var connection = mysql.createConnection({
@@ -15,6 +16,28 @@ var connection = mysql.createConnection({
 
   connection.connect(function (err) { 
       if(err) throw err;
-      console.log(`connected as id ${connection.threadId}`)
-      connection.end();
+      console.log(`You are connected as ${connection.threadId}`)
+
+    showAllProd();
    });
+
+   function showAllProd() {
+    var sqlQuery = ("SELECT *FROM products");
+    connection.query(sqlQuery, function(err, res){
+        if (err) throw err;
+        var table = new Table({
+            head: ['ID', 'Product', 'Department', 'Price', 'In Stock'],
+            // colWidths: [10, 30, 15, 10, 10, 15]
+          });
+          for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
+          }
+          console.log(table.toString() + "\n");
+        });
+        connection.end();
+    }
+   
+
+
+
+
