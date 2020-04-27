e96a9b6cb80f266d0554625297d867e2437931f5
John Pendergrass <john.pendergrass@me.com>
2020-04-27 16:10:17 -0500

    completed readme and linked video demonstration

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # online-shop-mysql
-Homework for week 12 
+Repository for linking a MySQL database with a command line node shpping app that takes in parameters and updates a database based on the user input of what the customer wants to buy, e.g. subtracting inventory of each product. 
+
+
+## Author
+John Pendergrass
+
+## Purpose of app
+The purpose was to use "backend" technologies such as Node.js along with NPM (Node Package Manager) libraries in tandem with a relational database management system in order to create an online shop. 
+
+## App organization
+### `optionMenu`
+
+  `node bamazon.js`
+
+   * This will give the customer the option of making a purchase or exiting the program. If the customer chooses to make a purchase, they will continue on. If not, the connection will end. 
+
+### `showAllProd`
+
+  Using this funtion, the customer will then see a table with columns that represent the:
+   
+    ```
+      * ID
+      * Product
+      * Department name
+      * Price
+      * Quantity in stock
+    ```
+
+### `runShop`
+
+  `node liri.js movie-this <movie name here>`
+
+  * This function then asks the customer the ID of the item they would like to buy and the quantity. Then the customer will be presented with a confimation question. If the answer is 'no', the customer is taken back to the beginning of runShop. If the customer answers 'yes', they will then continue through the function which will present:
+
+    ```
+      * the item they bought.
+      * the number of items bought.
+      * and the total retail price. 
+    ```
+
+## Instructions
+1. To start the program type in:  `node bamazon.js` in the terminal. 
+2. To make a purchase, hit enter. If not, use the arrow keys to select exit and hit enter.
+3. If you made the choice to make a purchase, then you will be presented with a table of items for sale.
+4. Choose the item id and write it in and hit enter. 
+5. Then choose the amount you would like to purchase. 
+6. You will be asked to confirm: If you would like to change your order, choose 'no' and start over. If you choose yes, you will be presented with the number of items and the total price. 
+
+## Screenshots and videos
+Link to the video: https://1drv.ms/v/s!AsTLbKRukIAN9DYEzz5MLQEZhEWW?e=ngSTbX
+
+## Links
+* Link to video: https://1drv.ms/v/s!AsTLbKRukIAN9DYEzz5MLQEZhEWW?e=ngSTbX
+* Link to repository: https://github.com/JohnWP8253/online-shop-mysql
+
+## Tech used
+    * JavaScript
+    * Node.js
+    * MySQL
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,41 +24,43 @@ connection.connect(function (err) {
 });
 
 function optionMenu() {
-  console.log((chalk.green("Welcome to Bamazon!")))
-  inquirer.prompt({
+  console.log(chalk.green("Welcome to Bamazon!"));
+  inquirer
+    .prompt({
       type: "list",
       message: "What would you like to do?",
       name: "choice",
-      choices: ["Make a purchase", "Exit"]
-  }).then(function (response) {
+      choices: ["Make a purchase", "Exit"],
+    })
+    .then(function (response) {
       switch (response.choice) {
-          case "Make a purchase":
-              showAllProd();
-              break;
-          case "Exit":
-              connection.end();
-              console.log("You have exited the program. Please come again!")
-              break;
+        case "Make a purchase":
+          showAllProd();
+          break;
+        case "Exit":
+          connection.end();
+          console.log("You have exited the program. Please come again!");
+          break;
       }
-  })
+    });
 }
 
 function showAllProd() {
-  var sqlQuery = "SELECT *FROM products";
+  var sqlQuery = "SELECT * FROM products";
   connection.query(sqlQuery, function (err, res) {
     if (err) throw err;
     var greeting = chalk.yellow`\n Here are the current products on sale today!\n`;
     console.log(greeting);
     var table = new Table({
       head: ["ID", "Product", "Department", "Price", "In Stock"],
-      // colWidths: [10, 30, 15, 10, 10, 15]
+      // colWidths: [10, 30, 15, 10, 10, 10]
     });
     for (var i = 0; i < res.length; i++) {
       table.push([
         res[i].item_id,
         res[i].product_name,
         res[i].department_name,
-        "$" + res[i].price,
+        res[i].price,
         res[i].stock_quantity,
       ]);
     }
@@ -95,33 +97,61 @@ function runShop() {
       },
       {
         type: "confirm",
-        message: chalk.green `Is this correct?`,
-        name: 'confirmation',
-        default: true
+        message: chalk.green`Is this correct?`,
+        name: "confirmation",
+        // Customer can just press the enter key for yes without typing y
+        default: true,
       },
     ])
     .then(function (userResponse) {
-     
-      connection.query("SELECT * FROM products WHERE ?", {
-        item_id: userResponse.itemID
-      }, function (error, response) {
-        console.log(`\nYou have chosen to buy ${userResponse.units} ${response[0].product_name}(s).\n`);
-        if (userResponse.units > response[0].stock_quantity) {
-          console.log(chalk.red`\nOh no. We're all out of that item. Apologies.\n`)
-          optionMenu();
-        } else {
-          console.log(`\nOrder processing...\n`);
-          var totalCost = userResponse.units * response[0].price;
-          var updateStock = response[0].stock_quantity - userResponse.units;
-          var update = "UPDATE products SET stock_quantity = " + updateStock + " WHERE item_id = " + userResponse.itemID;
-          connection.query(update, function (error, response){
+      //  query the database for all items being sold
+      if (userResponse.confirmation === false) {
+        console.log(
+          `Please re-enter your the item and quantity you want to purchase. `
+        );
+        runShop();
+      } else {
+        connection.query(
+          "SELECT * FROM products WHERE ?",
+          {
+            // assigning the item-id column with the userResponse item chosen
+            item_id: userResponse.itemID,
+          },
+          function (error, response) {
             if (error) throw error;
-            console.log(chalk.cyan`\nYour purchase is complete! Your total comes to: $${totalCost}.\n\n`);
-            console.log(chalk.cyan`We look forward to your next visit!`);
-            connection.end();
-          })
-        }
+
+            // inform customer of how many units they ordered
+
+            if (userResponse.units > response[0].stock_quantity) {
+              console.log(
+                chalk.red`\nOh no. We're all out of that item. Apologies.\n`
+              );
+              optionMenu();
+            } else {
+              console.log(
+                `\nYou have chosen to buy ${userResponse.units} ${response[0].product_name}(s).\n`
+              );
+              console.log(`\nOrder processing...\n`);
+              var totalCost = userResponse.units * response[0].price;
+              var updateStock = response[0].stock_quantity - userResponse.units;
+              var update =
+                "UPDATE products SET stock_quantity = " +
+                updateStock +
+                " WHERE item_id = " +
+                userResponse.itemID;
+              connection.query(update, function (error, response) {
+                if (error) throw error;
+
+                console.log(
+                  chalk.cyan`\nYour purchase is complete! Your total comes to\n`
+                );
+                console.log(`$${totalCost}\n`);
+                console.log(chalk.cyan`We look forward to your next visit!`);
+                connection.end();
+              });
+            }
+          }
+        );
       }
-      )
     });
 }
